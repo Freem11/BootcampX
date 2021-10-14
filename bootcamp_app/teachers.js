62ddfa33247cd99574de925b5de96328b051a19c
Freem11 <freem1985@gmail.com>
2021-10-13 17:47:28 -0700

    Db connections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,19 +8,19 @@ const pool = new Pool({
 });
 
 const mnth = process.argv[2];
-const lmt = process.argv[3];
-
 
 pool.query(`
-SELECT students.id, students.name as student, cohorts.name as cohort
-FROM students
+SELECT distinct(teachers.name) AS teacher, cohorts.name AS cohort
+FROM assistance_requests 
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
 JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${mnth}%'
-LIMIT '${lmt || 5}';
+ORDER BY teachers.name;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort} : ${user.teacher}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
